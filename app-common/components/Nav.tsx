57c8d86d3ad82f5bff8b13a7b1623d5afa511e2d
Bnaya Eshet <3382669+bnayae@users.noreply.github.com>
2021-12-07 14:49:48 +0200

    [ADD] component + utils

---
+++ app-common/components/Nav.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './Nav.module.css'; 
+
+
+export const Nav = () => {
+    return (
+        <nav className={styles.nav}>
+            Nav
+        </nav>
+    )
+}
